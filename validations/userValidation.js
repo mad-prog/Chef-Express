36675e22182ba62f16c2874605d3c2c6d0ff8ab4
Madeleine Elliot <81318490+mad-prog@users.noreply.github.com>
2021-06-04 11:36:19 +0200

    RegEx joi validation simplification

--- validations/userValidation.js
+++ validations/userValidation.js
@@ -2,18 +2,14 @@ const Joi = require("joi");
 
 exports.insertUserSchema = Joi.object({
   email: Joi.string().email({ minDomainSegments: 2 }).required(),
-  password: Joi.string()
-    .regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})")
-    .required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
   //min 1 lowercase, min 1 uppercase, min 1 numeric, min 1 special character, min 8 characters long
   name: Joi.string().required(),
 });
 
 exports.updateUserSchema = Joi.object({
   email: Joi.string().email({ minDomainSegments: 2 }),
-  password: Joi.string().regex(
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
-  ),
-  //min 1 lowercase, min 1 uppercase, min 1 numeric, min 1 special character, min 8 characters long
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+
   name: Joi.string(),
 });
